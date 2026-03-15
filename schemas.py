46c1d095b738f4915e392a6a781584fb863c8204
adhithyalakshman <adhithyalakshman.off@gmail.com>
2026-03-15 17:41:23 +0530

    updated DB from SQL based one to postgre based one

--- schemas.py
+++ schemas.py
@@ -4,10 +4,12 @@ from sqlalchemy import Column, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
 
-DATABASE_URL = "sqlite:///./main.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, pool_size=20,
-    max_overflow=10)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
